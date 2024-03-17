1c8a1c12f42de4064838c0d38af28450050b918c
arindomboruah <arindomboruah69@gmail.com>
2024-03-17 19:16:32 +0530

    Nigg

--- Loops.c
+++ Loops.c
@@ -1,27 +1,52 @@
-// #include<stdio.h>
-// int main()
-// {
-//     for(int i=1; i<=10; i++)
-//     {
-//         if(i==6)
-//         {
-//             continue;
-//         }
-//         printf("%d\n",i);
-//     }
-//     return 0;
-// }
-// 
-// 
-// 
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
-    int sum=0;
-    for(int i=5; i<=50; i++)
-    {
-        sum=sum+i;
-    }
-    printf("The sum is:%d",sum);
+    for(int i=1; i<=10; i++)
+  {
+    printf("*");
+  }
+  printf("\n");
+  for(int i=1; i<=1; i++)
+  {
+    printf("****  ****");
+  }
+  printf("\n");
+  for(int i=1; i<=1; i++)
+  {
+    printf("***    ***");
+  }
+  printf("\n");
+  for(int i=1; i<=1; i++)
+  {
+    printf("**      **");
+  }
+  printf("\n");
+  for(int i=1; i<=1; i++)
+  {
+    printf("*        *");
+  }
+  printf("\n");
+   for(int i=1; i<=1; i++)
+  {
+    printf("**      **");
+  }
+  printf("\n");
+   for(int i=1; i<=1; i++)
+  {
+    printf("***    ***");
+  }
+  printf("\n");
+   for(int i=1; i<=1; i++)
+  {
+    printf("****  ****");
+  }
+  printf("\n");
+   for(int i=1; i<=10; i++)
+  {
+    printf("*");
+  }
+  printf("\n");
+    getch ();
     return 0;
 }
--- tempCodeRunnerFile.c
+++
@@ -1,2 +0,0 @@
-
-        fact=fact*i;
